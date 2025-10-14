7d0292597328fdf336172e12e4af6e3569f8b421
andrea <carillagonzalezandrea@gmail.com>
2025-10-14 16:35:50 +0200

    cambio de nombre tresor

--- js/main.js
+++ js/main.js
@@ -203,7 +203,7 @@ const projects = [
     name: "<3or",
     category: "eventos",
     status: true,
-    url: "proyectos/<3or>/<3or>.html",
+    url: "proyectos/tresor/tresor.html",
   },
   {
     name: "high tide",
